369d37d81aab8a38aa78b2feab8f3fa875ca14e8
Rogue Elephant <jup.trandell@gmail.com>
2020-08-01 10:12:57 +0100

    Initial setup for debugging in vs code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hello-Rust
+Playground project for getting Rust up and running on Windows with VSCode.
+
+You may have to go into user settings and set allow breakpoints in any file.
+
+Then add a breakpoint and hit f5 to run.
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{stdout, BufWriter};
+use ferris_says::say;
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello World!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
