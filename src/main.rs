9be2c5a6c2d18ab03074f13d6c7d78aa3f83aed9
Rogue Elephant <jup.trandell@gmail.com>
2020-08-03 14:41:08 +0100

    Schedule clash initial code setup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,56 @@
-use std::io::{stdout, BufWriter};
-use ferris_says::say;
+use chrono::{DateTime, TimeZone, Utc};
+
+struct ScheduleEvent {
+    title: String,
+    start_time: DateTime<Utc>,
+    end_time: DateTime<Utc>,
+}
+#[allow(dead_code)]
+impl ScheduleEvent {
+    fn new(title: String, start: DateTime<Utc>, end: DateTime<Utc>) -> ScheduleEvent {
+        ScheduleEvent {
+            title: title,
+            start_time: start,
+            end_time: end,
+        }
+    }
+}
+
+fn find_schedule_clash(events: &[ScheduleEvent]) {
+    for (initial_index, initial_event) in events.iter().enumerate() {
+        for (comparison_index, comparison_event) in events.iter().enumerate() {
+            if initial_index != comparison_index
+                && (comparison_event.start_time >= initial_event.start_time
+                    && comparison_event.start_time < initial_event.end_time)
+            {
+                println!(
+                    "Meeting Clash Found between {initialEventTitle} and {comparisonEventTitle}.",
+                    initialEventTitle = initial_event.title,
+                    comparisonEventTitle = comparison_event.title
+                );
+            }
+        }
+    }
+}
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello World!");
-    let width = message.chars().count();
+    let events: [ScheduleEvent; 3] = [
+        ScheduleEvent::new(
+            String::from("Meeting 1"),
+            Utc.ymd(2020, 2, 2).and_hms(9, 30, 0),
+            Utc.ymd(2020, 2, 2).and_hms(10, 0, 0),
+        ),
+        ScheduleEvent::new(
+            String::from("Meeting 2"),
+            Utc.ymd(2020, 2, 2).and_hms(10, 30, 0),
+            Utc.ymd(2020, 2, 2).and_hms(11, 30, 0),
+        ),
+        ScheduleEvent::new(
+            String::from("Meeting 3"),
+            Utc.ymd(2020, 2, 2).and_hms(9, 45, 0), // Should clash with Meeting 1
+            Utc.ymd(2020, 2, 2).and_hms(12, 00, 0), // Should clash with Meeting 2
+        ),
+    ];
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    find_schedule_clash(&events);
 }
